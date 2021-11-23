cace627bb60ea9959a77d3e09fcf2b23d66d19df
SeongeunYang <yse09@naver.com>
2021-11-24 04:53:50 +0900

    상세 페이지 불러오기 기능 구현, 댓글 UI만 구현 댓글 기능 구현 필요

--- src/main/java/com/diddl/minipjt3/controller/UserController.java
+++ src/main/java/com/diddl/minipjt3/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.diddl.minipjt3.controller;
 
 import com.diddl.minipjt3.dto.UserInfoDto;
+import com.diddl.minipjt3.security.UserDetailsImpl;
 import com.diddl.minipjt3.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //컨트롤러로 선언 -> Bean등록
 @RequiredArgsConstructor //final 생성자 자동으로 해주는 어노테이션
@@ -15,7 +16,10 @@ public class UserController {
 
     // 로그인 페이지 요청 시
     @GetMapping("/user/login")
-    public String login() {
+    public String login(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if(userDetails != null){
+            return "prelogin";
+        }
         return "login";
     }
 
@@ -27,7 +31,11 @@ public class UserController {
 
     // 회원가입 요청 시
     @PostMapping("/user/signup")
-    public String registerUser(UserInfoDto userInfoDto) {
+    public String registerUser(UserInfoDto userInfoDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if(userDetails != null){
+
+            return "prelogin";
+        }
         userService.registerUser(userInfoDto);
         return "redirect:/user/login";
     }
---
+++ src/main/java/com/diddl/minipjt3/controller/UsermentController.java
@@ -0,0 +1,8 @@
+package com.diddl.minipjt3.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UsermentController {
+
+}
